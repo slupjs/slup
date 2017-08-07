559dc378f01e8c58e747795026c799735cb3ad99
Gejsi <vgejsi@gmail.com>
2017-08-07 22:40:05 +0200

    :package: Add ripple package

---
+++ packages/Ripple/src/index.js
@@ -0,0 +1,180 @@
+import { h, Component } from 'preact'
+import { bind } from 'decko'
+
+class Ripple extends Component {
+  state = {
+    ripples: []
+  }
+
+  componentDidMount() {
+    this.parent = this.ripple.parentElement
+
+    const { parent, handleMouseDown } = this
+    parent.addEventListener('mousedown', handleMouseDown)
+  }
+
+  componentWillUnmount() {
+    const { parent, handleMouseDown } = this
+    parent.removeEventListener('mousedown', handleMouseDown)
+  }
+
+  @bind
+  createListener(id) {
+    const { parent, startRemoval }  = this
+    const { ripples } = this.state
+
+    const handler = function() {
+      ripples[id].removable = true
+
+      // This will just work if the
+      // animation has ended
+      startRemoval(id)
+      parent.removeEventListener('mouseup', handler)
+    }
+
+    parent.addEventListener('mouseup', handler)
+  }
+
+  @bind
+  startRemoval(id) {
+    const { ripples } = this.state
+    const { opacityTiming } = this.props
+
+    if(!ripples[id].animationEnded || !ripples[id].removable)
+      return false
+
+    // If we can remove it we first start
+    // By decreasing the opacity to 0 and
+    // Then after the animation finishes we
+    // Remove the node
+    const _styles = {
+      ...ripples[id].styles,
+      opacity: '0'
+    }
+
+    ripples[id].styles = _styles
+    this.setState({ ripples })
+
+    setTimeout(e => this.removeRipple(id), opacityTiming || 250)
+  }
+
+  @bind
+  removeRipple(id) {
+    const { ripples } = this.state
+
+    console.log("Removig ripple: #", id)
+    ripples[id] = undefined
+    this.setState({ ripples })
+  }
+
+  @bind
+  animationEnded(id) {
+    const { ripples } = this.state
+
+    ripples[id].animationEnded = true
+    this.setState({ ripples })
+
+    // This will just work if the user has
+    // fired the mouseup event
+    this.startRemoval(id)
+  }
+
+  @bind
+  startScaling(id) {
+    const { ripples } = this.state
+    const { scaleTiming } = this.props
+    const self = this
+
+    setTimeout(e => this.animationEnded(id), scaleTiming || 500)
+
+    const _styles = {
+      ...ripples[id].styles,
+      transform: 'scale(100)'
+    }
+
+    ripples[id].styles = _styles
+    setTimeout( () => self.setState({ ripples }), 1)
+
+  }
+
+  @bind
+  createNewRipple(x, y) {
+    const id      = this.state.ripples.length
+    const { rippleStyles } = this.getStyles()
+    const { parent } = this
+    const { ripples, timeouts } = this.state
+    const self = this
+
+    const ripple = {
+      animationEnded: false,
+      removable: false,
+      styles: {
+        ...rippleStyles,
+        top: y - (rippleStyles.height / 2),
+        left: x - (rippleStyles.width / 2)
+      }
+    }
+
+    ripples.push(ripple)
+    this.setState({ ripples })
+
+    this.createListener(id)
+    this.startScaling(id)
+  }
+
+  @bind
+  handleMouseDown(event) {
+    event.stopPropagation()
+    const { offsetX, offsetY } = event
+
+    this.createNewRipple(offsetX, offsetY)
+
+  }
+
+  @bind
+  getStyles() {
+    const {
+      startingSize,
+      scaleTiming,
+      opacityTiming,
+      easing
+    } = this.props
+
+    const styles = {
+      zIndex: -1,
+      position: 'absolute',
+      left: 0, top: 0,
+      bottom: 0, right: 0,
+      overflow: 'hidden',
+      background: 'red',
+
+      rippleStyles: {
+        zIndex: -1,
+        position: 'absolute',
+        height: startingSize || 10,
+        width: startingSize || 10,
+        background: 'gray',
+        opacity: .35,
+        transition: `
+          transform ${scaleTiming / 100 || .5}s,
+          opacity ${opacityTiming / 100 || .25}s
+        `,
+        transitionTimingFunction: easing || 'ease-in-out',
+        borderRadius: '50%'
+      }
+    }
+
+    return styles
+  }
+
+  render() {
+    const styles = this.getStyles()
+    const { ripples } = this.state
+
+    return <div ref={e => this.ripple = e} style={styles} >
+      {ripples.filter(Boolean).map(({ styles }, i) => {
+        return <span key={i} style={styles} />
+      })}
+    </div>
+  }
+}
---
+++ packages/Ripple/webpack.config.js
@@ -0,0 +1,41 @@
+const webpack           = require('webpack')
+const BabiliPlugin      = require("babili-webpack-plugin")
+const { join }          = require('path')
+
+let config = {
+	entry: join(__dirname, 'src', 'index'),
+
+	output: {
+		path: __dirname,
+		filename: 'dist.js'
+	},
+
+	resolve: {
+		extensions: [ '.js' ]
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin()
+	],
+
+	devtool: 'source-map'
+}
+
+if(process.env.NODE_ENV == 'production') {
+	config.plugins.push(new BabiliPlugin())
+  config.plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
+
+	config.devtool = 'source-map'
+}
+
+module.exports = config
