44390455eb6fd802676546325d3b67fb8bb5695f
Luca <LucaT1@users.noreply.github.com>
2017-08-15 00:42:46 +0200

    :ambulance: Provide default values

--- packages/Navbar/src/index.js
+++ packages/Navbar/src/index.js
@@ -9,15 +9,17 @@ export class Navbar extends Component {
     const { background, color } = this.props
 
     const styles = {
-      boxShadow: `0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-      0px 4px 5px 0px rgba(0, 0, 0, 0.14),
-      0px 1px 10px 0px rgba(0, 0, 0, 0.12)`,
+      boxShadow: `
+        0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+        0px 4px 5px 0px rgba(0, 0, 0, 0.14),
+        0px 1px 10px 0px rgba(0, 0, 0, 0.12)
+      `,
       height: 64,
       display: 'flex',
       alignItems: 'center',
       padding: '0 16px',
-      background: background,
-      color: color
+      background: background || 'transparent',
+      color: color || '#FFFFFF'
     }
 
     return styles
