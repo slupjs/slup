6b67afa5b283ec8d756f904c3b19e0b34d05b797
Gejsi <vgejsi@gmail.com>
2017-08-14 23:43:22 +0200

    Small bug fix

--- packages/Navbar/src/index.js
+++ packages/Navbar/src/index.js
@@ -9,9 +9,9 @@ export class Navbar extends Component {
     const { background, color } = this.props
 
     const styles = {
-      boxShadow: '0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+      boxShadow: `0px 2px 4px -1px rgba(0, 0, 0, 0.2),
       0px 4px 5px 0px rgba(0, 0, 0, 0.14),
-      0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
+      0px 1px 10px 0px rgba(0, 0, 0, 0.12)`,
       height: 64,
       display: 'flex',
       alignItems: 'center',
