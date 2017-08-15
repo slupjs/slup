8eb4697976ff422553f6762e410285c6199efbe2
Luca <LucaT1@users.noreply.github.com>
2017-08-15 13:23:31 +0200

    :lipstick: Fix indentations

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,36 @@ const webpack           = require('webpack')
 const { join }          = require('path')
 
 module.exports = {
-	entry: join(__dirname, 'src', 'index'),
+  entry: join(__dirname, 'src', 'index'),
 
-	output: {
-		path: __dirname,
-		filename: 'dist.js'
-	},
+  output: {
+    path: __dirname,
+    filename: 'dist.js'
+  },
 
-	resolve: {
-		extensions: [ '.js' ],
+  resolve: {
+    extensions: [ '.js' ],
     alias: {
       '@slup/ripple': join(__dirname, 'packages', 'Ripple', 'src', 'index'),
       '@slup/slider': join(__dirname, 'packages', 'Slider', 'src', 'index'),
       '@slup/buttons': join(__dirname, 'packages', 'Buttons', 'src', 'index')
     }
-	},
+  },
 
-	module: {
-		loaders: [
-			{
-				test: /\.js?$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			}
-		]
-	},
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
 
-	plugins: [
-		new webpack.optimize.ModuleConcatenationPlugin()
-	],
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
 
-	devtool: 'source-map',
-	target: 'web'
+  devtool: 'source-map',
+  target: 'web'
 }
