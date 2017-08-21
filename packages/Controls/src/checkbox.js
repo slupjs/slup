94e1e72ba08b32718d1f4a996674ad0b37a07244
Gejsi <vgejsi@gmail.com>
2017-08-21 23:48:36 +0200

    :lipstick: Added disabled feature

--- packages/Controls/src/checkbox.js
+++ packages/Controls/src/checkbox.js
@@ -8,8 +8,12 @@ const Box = styled.div`
   height: ${props => props.size || 18}px;
   border-radius: 2px;
   box-sizing: border-box;
-  border: 2px solid ${props => props.disabled ? '#9e9e9e' : props.checked ? 'teal' : '#616161'};
-  background-color: ${props => props.disabled && props.checked ? '#9e9e9e' : props.checked ? 'teal' : 'transparent'};
+  border: 2px solid ${props => props.disabled ? '#9e9e9e'
+    : props.checked ? 'teal'
+    : '#616161'};
+  background-color: ${props => props.disabled && props.checked ? '#9e9e9e'
+    : props.checked ? 'teal'
+    : 'transparent'};
   transition: background-color 300ms cubic-bezier(0.4, 0.0, 0.2, 1),
     border-color 200ms linear;
   outline: none;
@@ -34,17 +38,18 @@ const Box = styled.div`
     left: -2px;
     opacity: ${props => props.checked ? '1' : '0'};
     transform-origin: 0 0;
-    transition: transform 200ms cubic-bezier(0.4, 0.0, 0.2, 1),
-      opacity 500ms cubic-bezier(0.4, 0.0, 0.2, 1);
+    transition: opacity 500ms cubic-bezier(0.4, 0.0, 0.2, 1);
   }
 
   &::after {
+    transition: transform 50ms cubic-bezier(0.4, 0.0, 0.2, 1);
     transform: translate(${props => props.size / 5.3 || 3.5}px, ${props => props.size / 2.6 || 7.2}px)
       rotate(44.91deg)
       ${props => props.checked ? 'scale(.4,.1)' : 'scale(.1, .1)'}
   }
 
   &::before {
+    transition: transform 200ms cubic-bezier(0.4, 0.0, 0.2, 1);
     transform: translate(${props => props.size / 2.57 || 7}px, ${props => props.size / 1.6 || 11}px)
       rotate(-45deg)
       ${props => props.checked ? 'scale(.6,.1)' : 'scale(.1, .1)'}
--- packages/Controls/src/switch.js
+++ packages/Controls/src/switch.js
@@ -7,10 +7,19 @@ const Bar = styled.div`
   width: 36px;
   height: 14px;
   border-radius: 8px;
-  background: ${props => props.checked ? 'rgba(0, 150, 136, .7)' : 'rgba(0, 0, 0, .38)'};
+  background: ${props => props.disabled ? 'rgba(0, 0, 0, .12)'
+    : props.checked ? 'rgba(0, 150, 136, .7)'
+    : 'rgba(0, 0, 0, .38)'};
   position: relative;
   transition: background 200ms linear;
-  cursor: pointer;
+  cursor: ${props => props.disabled
+    ? 'not-allowed'
+    : 'pointer'
+  };
+  pointer-events: ${props => props.disabled
+    ? 'none'
+    : 'auto'
+  };
 `
 
 const Thumb = styled.div`
@@ -21,7 +30,7 @@ const Thumb = styled.div`
     0px 1px 1px 0px rgba(0, 0, 0, 0.14),
     0px 1px 3px 0px rgba(0, 0, 0, 0.12);
   transition: transform 200ms cubic-bezier(0.4, 0.0, 0.2, 1), background 200ms linear;
-  background: ${props => props.checked ? 'rgb(0, 150, 136)' : '#FAFAFA'};
+  background: ${props => props.disabled ? '#BDBDBD' : props.checked ? 'rgb(0, 150, 136)' : '#FAFAFA'};
   position: absolute;
   top: -3px; left: -2px;
   transform: ${props => props.checked ? 'translateX(100%)' : 'translateX(0)'};
@@ -33,7 +42,10 @@ export class Switch extends Component {
   render(props) {
     return(
       <Bar {...props} onClick={props.onChange}>
-        <Thumb onClick={props.onChange} checked={props.checked} />
+        <Thumb
+          onClick={props.onChange}
+          checked={props.checked}
+          disabled={props.disabled} />
       </Bar>
     )
   }
