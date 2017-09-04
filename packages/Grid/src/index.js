f0aa93cc5af62a2a909b12991dd5e8a6c7d854d5
Luca <LucaT1@users.noreply.github.com>
2017-09-04 19:23:38 +0200

    :sparkles: Sanitize measures instead of arbitrary using pixels

--- packages/Grid/src/index.js
+++ packages/Grid/src/index.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+const sanitize = (value) => typeof value == 'number' ? value + 'px' : value
+
 export const Grid = styled.div`
   box-sizing: border-box;
   display: flex;
@@ -29,24 +31,24 @@ export const Col = styled.div`
   height: auto;
   transition: margin 150ms linear;
   flex-basis: ${props => 100 / 12 * props.sm || 0}%;
-  margin-left: ${props => props.offset || 0}px;
-  margin-right: ${props => props.pull || 0}px;
+  margin-left: ${props => sanitize(props.offset || 0)};
+  margin-right: ${props => sanitize(props.pull || 0)};
 
   @media only screen and (min-width: 480px) {
     flex-basis: ${props => 100 / 12 * (props.md || props.sm || 0)}%;
-    margin-left: ${props => props.offset_md || 0}px;
-    margin-right: ${props => props.pull_md || 0}px;
+    margin-left: ${props => sanitize(props.offset_md || 0)};
+    margin-right: ${props => sanitize(props.pull_md || 0)};
   }
 
   @media only screen and (min-width: 960px) {
     flex-basis: ${props => 100 / 12 * (props.lg || props.sm || 0)}%;
-    margin-left: ${props => props.offset_lg || props.offset_md || 0}px;
-    margin-right: ${props => props.pull_lg || props.pull_md || 0}px;
+    margin-left: ${props => sanitize(props.offset_lg || props.offset_md || 0)};
+    margin-right: ${props => sanitize(props.pull_lg || props.pull_md || 0)};
   }
 
   @media only screen and (min-width: 1280px) {
     flex-basis: ${props => 100 / 12 * (props.xl || props.sm || 0)}%;
-    margin-left: ${props => props.offset_xl || props.offset_lg || 0}px;
-    margin-right: ${props => props.pull_xl || props.pull_lg || 0}px;
+    margin-left: ${props => sanitize(props.offset_xl || props.offset_lg || 0)};
+    margin-right: ${props => sanitize(props.pull_xl || props.pull_lg || 0)};
   }
 `
