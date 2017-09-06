46625e296c45293adf8e57f77c3788d48b4ae2a8
Gejsi <vgejsi@gmail.com>
2017-09-06 15:28:05 +0200

    :hammer: Refactored the discrete indicator

--- packages/Slider/src/indicator.js
+++ packages/Slider/src/indicator.js
@@ -1,62 +1,41 @@
 import Inferno   from 'inferno'
 import Component from 'inferno-component'
-import { bind }  from 'decko'
+import styled    from 'styled-components'
+
+const Discrete = styled.div`
+  position: absolute;
+  left: ${props => (props.value / props.max) * 100}%;
+  bottom: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #2196F3;
+  color: white;
+  border-radius: 50%;
+  font-size: 12px;
+  transition: transform 300ms cubic-bezier(0.4, 0.0, 0.2, 1);
+  user-select: none;
+  height: 30px; width: 30px;
+  transform-origin: bottom;
+  transform: translateX(-50%) ${props => props.focus ? 'scale(1)' : 'scale(0)'};
+
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: -6.3px;
+    width: 0; height: 0;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-top: 10px solid #2196F3;
+  }
+`
 
 export class Indicator extends Component {
-
-  @bind
-  getStyles() {
-    const {
-      value,
-      max,
-      color,
-      background,
-      focus
-    } = this.props
-
-    const progress = (value / max) * 100
-
-    const styles = {
-      position: 'absolute',
-      left: progress + '%',
-      bottom: 14,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      background: background || '#2196F3',
-      color: color || 'rgba(255, 255, 255, .87)',
-      borderRadius: '48.8%',
-      textTransform: 'uppercase',
-      fontSize: 12,
-      transition: 'transform .3s cubic-bezier(0.4, 0.0, 0.2, 1)',
-      userSelect: 'none',
-      height: 32,
-      width: 32,
-      transformOrigin: 'bottom',
-      transform: `translateX(-50%) ${focus ? 'scale(1, 1)' : 'scale(0, 0)'}`,
-
-      triangle: {
-        position: 'absolute',
-        bottom: -7,
-        width: 0,
-        height: 0,
-        borderLeft: '12px solid transparent',
-        borderRight: '12px solid transparent',
-        borderTop: `12px solid ${background || '#2196F3'}`,
-      }
-    }
-
-    return styles
-  }
-
-  render({ value, max }) {
-    const styles = this.getStyles()
-
+  render(props) {
     return(
-      <div style={styles}>
-        <span>{Math.floor((value / max) * 100)}</span>
-        <div style={styles.triangle}></div>
-      </div>
+      <Discrete {...props}>
+        <span>{Math.floor(props.value)}</span>
+      </Discrete>
     )
   }
 }
