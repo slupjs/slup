54942c9566a7f547be1415f164320c3e8327ca02
Luca <LucaT1@users.noreply.github.com>
2017-09-14 00:19:07 +0200

    :lipstick: Inherit color from the parent instead of using background

--- packages/Tabs/src/tabContainer.js
+++ packages/Tabs/src/tabContainer.js
@@ -13,7 +13,7 @@ const Container = styled.div`
   justify-content: ${props => props.center ? 'center' : 'flex-start'};
   background: ${props => props.primary
     ? darken(0.02, props.theme.primary || lightTheme.primary)
-    : darken(0.02, props.theme.background || lightTheme.background)
+    : 'inherit'
   };
 `
 
