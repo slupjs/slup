5108d2931defe36e688db6c5f8075f0816d79669
Luca <LucaT1@users.noreply.github.com>
2017-09-20 20:10:11 +0200

    :arrow_down: Lower ripple package size

--- packages/Ripple/webpack.config.js
+++ packages/Ripple/webpack.config.js
@@ -1,6 +1,6 @@
-const webpack           = require('webpack')
-const BabiliPlugin      = require("babili-webpack-plugin")
-const { join }          = require('path')
+const webpack = require('webpack')
+const BabiliPlugin = require("babili-webpack-plugin")
+const { join } = require('path')
 
 let config = {
   entry: join(__dirname, 'src', 'index'),
@@ -11,8 +11,37 @@ let config = {
   },
 
   resolve: {
-    extensions: [ '.js' ]
+    extensions: ['.js'],
+    alias: {
+      'react': 'inferno-compat',
+      'react-dom': 'inferno-compat'
+    }
   },
+
+  externals: {
+    inferno: {
+      commonjs: 'inferno',
+      amd: 'inferno',
+      root: 'Inferno'
+    },
+    'inferno-component': {
+      commonjs: 'inferno-component',
+      amd: 'inferno-component',
+      root: 'Inferno.Component'
+    },
+    'styled-components': {
+      commonjs: 'styled-components',
+      amd: 'styled-components',
+      root: 'styled'
+    },
+    'polished': {
+      commonjs: 'polished',
+      amd: 'polished',
+      root: 'polished'
+    },
+  },
+
+  plugins: [],
 
   module: {
     loaders: [
@@ -24,15 +53,11 @@ let config = {
     ]
   },
 
-  plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin()
-  ],
-
   devtool: 'source-map',
   target: 'web'
 }
 
-if(process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV == 'production') {
   config.plugins.push(new BabiliPlugin())
   config.plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
 }
