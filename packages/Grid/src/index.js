0e9c4a6fff276110d9a61f95ed9f48b6c125cc5b
Gejsi <vgejsi@gmail.com>
2017-09-20 15:47:44 +0200

    :sparkles: Added the 'show' feature

--- packages/Grid/src/index.js
+++ packages/Grid/src/index.js
@@ -33,26 +33,26 @@ export const Col = styled.div`
   flex-basis: ${props => 100 / 12 * props.sm || 0}%;
   margin-left: ${props => sanitize(props.offset || 0)};
   margin-right: ${props => sanitize(props.pull || 0)};
-  display: ${props => props.hide_sm ? 'none' : 'initial'};
+  display: ${props => props.hide_sm && !props.show_sm ? 'none' : 'initial'};
 
   @media only screen and (min-width: 480px) {
     flex-basis: ${props => 100 / 12 * (props.md || props.sm || 0)}%;
     margin-left: ${props => sanitize(props.offset_md || 0)};
     margin-right: ${props => sanitize(props.pull_md || 0)};
-    display: ${props => (props.hide_md || props.hide_sm) ? 'none' : 'initial'};
+    display: ${props => props.hide_md && !props.show_md ? 'none' : 'initial'};
   }
 
   @media only screen and (min-width: 960px) {
     flex-basis: ${props => 100 / 12 * (props.lg || props.sm || 0)}%;
     margin-left: ${props => sanitize(props.offset_lg || props.offset_md || 0)};
     margin-right: ${props => sanitize(props.pull_lg || props.pull_md || 0)};
-    display: ${props => (props.hide_lg || props.hide_md || props.hide_sm) ? 'none' : 'initial'};
+    display: ${props => props.hide_lg && !props.show_lg ? 'none' : 'initial'};
   }
 
   @media only screen and (min-width: 1280px) {
     flex-basis: ${props => 100 / 12 * (props.xl || props.sm || 0)}%;
     margin-left: ${props => sanitize(props.offset_xl || props.offset_lg || 0)};
     margin-right: ${props => sanitize(props.pull_xl || props.pull_lg || 0)};
-    display: ${props => (props.hide_xl || props.hide_lg || props.hide_md || props.hide_sm) ? 'none' : 'initial'};
+    display: ${props => props.hide_xl && !props.show_xl ? 'none' : 'initial'};
   }
 `
