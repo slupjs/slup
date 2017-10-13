c9038f3686a3aa62046ca35101fc30774e7fc40a
Luca <LucaT1@users.noreply.github.com>
2017-10-13 14:43:01 +0200

    :sparkles: Use a global rollup config

--- packages/Theming/rollup.config.js
+++ packages/Theming/rollup.config.js
@@ -1,14 +1,10 @@
-/** Rollup plugins */
-import Min from 'rollup-plugin-babel-minify'
-import TS from 'rollup-plugin-typescript2'
-import CJS from 'rollup-plugin-commonjs'
-import RS from 'rollup-plugin-node-resolve'
+/** Impprt the base configuration */
+import extend from '../../rollup.base'
 
 /** Utils */
 import { join } from 'path'
-import { tmpdir } from 'os'
 
-export default {
+export default extend({
   /** Import path */
   input: join(__dirname, 'src', 'index.ts'),
 
@@ -21,26 +17,5 @@ export default {
 
     /** Name for the umd export */
     name: 'Slup.Theming'
-  },
-
-  external: [
-    'inferno-component', 
-    'inferno', 
-    'inferno-create-element',
-    '@slup/common'
-  ],
-
-  globals: {
-    'inferno-component': 'Inferno.Component',
-    'inferno': 'Inferno',
-    'inferno-create-element': 'Inferno.createElement',
-    '@slup/common': 'Slup.Common'
-  },
-
-  plugins: [
-    CJS(),
-    RS(),
-    TS({ cacheRoot: `${tmpdir()}/.rpt2_cache` }),
-    // Min({ comments: false })
-  ]
-}
+  }
+})
---
+++ rollup.base.js
@@ -0,0 +1,49 @@
+/** Rollup plugins */
+import Min from 'rollup-plugin-babel-minify'
+import TS from 'rollup-plugin-typescript2'
+import CJS from 'rollup-plugin-commonjs'
+import RS from 'rollup-plugin-node-resolve'
+
+/** Utils */
+import { join } from 'path'
+import { tmpdir } from 'os'
+
+const base = {
+  /** External dependecies, resolved via commonjs or AMD */
+  external: [
+    'inferno-component',
+    'inferno',
+    'inferno-create-element',
+    '@slup/common',
+    '@slup/theming'
+  ],
+
+  /** External dependecies, resolved via commonjs or AMD */
+  globals: {
+    /** Slup modules */
+    '@slup/common': 'Slup.Common',
+    '@slup/theming': 'Slup.Theming',
+    '@slup/buttons': 'Slup.Buttons',
+    '@slup/controls': 'Slup.Controls',
+    '@slup/grid': 'Slup.Grid',
+    '@slup/icons': 'Slup.Icons',
+    '@slup/navbar': 'Slup.Navbar',
+    '@slup/sidenav': 'Slup.Sidenav',
+    '@slup/slider': 'Slup.Slider',
+    '@slup/tabs': 'Slup.Tabs',
+    '@slup/typography': 'Slup.Typography',
+
+    /** Other externals */
+    'inferno-component': 'Inferno.Component',
+    'inferno': 'Inferno'
+  },
+
+  plugins: [
+    CJS(),
+    RS(),
+    TS({ cacheRoot: join(tmpdir(), '.rpt2_cache') }),
+    Min({ comments: false })
+  ]
+}
+
+export default (extend) => Object.assign({}, base, extend)
