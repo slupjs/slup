0ace511049d41f08932615f7868261cfb6e4ffd6
Luca <LucaT1@users.noreply.github.com>
2017-10-13 14:43:32 +0200

    :lipstick: Update types

--- packages/Theming/src/interfaces.ts
+++ packages/Theming/src/interfaces.ts
@@ -8,16 +8,47 @@ export interface ColorShade<Color> {
   2: Color
 }
 
+export type ColorSet = {
+  50: string
+  100: string
+  200: string
+  300: string
+  400: string
+  500: string
+  600: string
+  700: string
+  800: string
+  900: string
+  A100?: string
+  A200?: string
+  A400?: string
+  A700?: string
+}
+
 /**
  * Theme interface for the general structure
  */
 export interface _ITheme {
+  /**
+   * Color for the text strings on your page
+   */
   text: string | ColorShade<string>
 
+  /**
+   * Background for common components
+   */
   background: string | ColorShade<string>
 
+  /**
+   * Background color used in primary components
+   * ex. buttons, slider, etc
+   */
   primary: string | ColorShade<string>
 
+  /**
+   * Background color used in secondary components when prop is given
+   * ex. buttons, slider, etc
+   */
   secondary: string | ColorShade<string>
 }
 
@@ -31,6 +62,9 @@ export interface IProps {
   children: any[]
 }
 
+/**
+ * Shett class helper to create css styleshetts
+ */
 export interface ISheet {
   
 }
