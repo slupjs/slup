7c2bbd84e3dbbd9c4f68fe1f5251c228509cc4c5
Gejsi <vgejsi@gmail.com>
2017-10-14 23:27:06 +0200

    :bug: Fixed bug with the conversion to hex

--- packages/Theming/src/hex.ts
+++ packages/Theming/src/hex.ts
@@ -1,11 +1,7 @@
 export const expandHex = (hex: string): string =>
   hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
 
-export const toHex = (h: any) => {
-  const hex = h.toString(16)
-  return hex.length == 1 ? '0' + hex : hex
-}
-
 export const rgbToHex = (r: any, g: any, b: any) => {
-  return '#' + toHex(r) + toHex(g) + toHex(b)
-} 
+  const rgb = b | (g << 8) | (r << 16)
+  return '#' + (0x1000000 + rgb).toString(16).slice(1)
+}
