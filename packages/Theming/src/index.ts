e038ddaa836244203c264a0e50239f7c28a3d995
Luca <LucaT1@users.noreply.github.com>
2017-10-14 16:59:16 +0200

    :package: Export SSR and themes

--- packages/Theming/src/index.ts
+++ packages/Theming/src/index.ts
@@ -2,3 +2,7 @@ export * from './provider'
 export * from './colors'
 export * from './styled'
 export * from './styles'
+export * from './server'
+
+export const lightTheme = {}
+export const darkTheme  = {}
