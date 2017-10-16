8cd5abd8a53a2b45c7fed394897f19702ed7b879
Gejsi <vgejsi@gmail.com>
2017-10-16 18:05:07 +0200

    :art: Added converter from rgb to hsl

--- packages/Theming/src/hex.ts
+++ packages/Theming/src/hex.ts
@@ -1,6 +1,6 @@
 /**
  * Dubles the HEX string passed, from 3 to 6 digits
- * 
+ *
  * @param hex 3-digit HEX code
  */
 export const expandHex = (hex: string): string =>
@@ -8,16 +8,16 @@ export const expandHex = (hex: string): string =>
 
 /**
  * Converts a rgb color into an HEX string
- * 
+ *
  * @param r The red value
  * @param g The green value
  * @param b The blue value
  */
-export const rgbToHex = (r: number, g: number, b: number): string => 
-  '#' + [r, g, b].map((x: number) => 
+export const rgbToHex = (r: number, g: number, b: number): string =>
+  '#' + [r, g, b].map((x: number) =>
     /** length of r/g/b -> hex */
-    x.toString(16).length === 1 
-      /** 
+    x.toString(16).length === 1
+      /**
        * If the x converted to 2-digit HEX is long 1 charter,
        * supply the first number as a 0
        */
@@ -28,3 +28,47 @@ export const rgbToHex = (r: number, g: number, b: number): string =>
   )
   /** Merge the array into a 6-digit HEX string */
   .join('')
+
+export const rgbToHsl = (r: number, g: number, b: number): object => {
+  /** Get the values from the rgb color */
+  let red: number   = r / 255
+  let green: number = g / 255
+  let blue: number  = b / 255
+
+  /** Take the max and the min from values */
+  let max: number   = Math.max(red, green, blue)
+  let min: number   = Math.min(red, green, blue)
+  let delta: number = max - min
+
+  /** Declare the hsl values that need to be set */
+  let h: number, s: number, l: number
+
+  /** Set the lightness */
+  l = Math.round(((max + min) / 2) * 100)
+
+  /** Set the saturation */
+  if (delta == 0) {
+    s = 0
+  } else {
+    s = Math.abs(delta / (1 - Math.abs((2 * l) - 1)))
+  }
+
+  /** Set the hue */
+  if (delta == 0) {
+    h = 0
+  } else if (red >= blue && blue > green) {
+    h = 60 * (6 - ((blue - green) / (red - green)))
+  } else if (max == green) {
+    h = 60 * (((blue - red) / delta) + 2)
+  } else if (max == blue) {
+    h = 60 * (((red - green) / delta) + 4)
+  }
+
+  /** Round the saturation value */
+  let _s = Math.round(s * 10000)
+  if (_s > 100) _s = 100
+
+  return {h, s: _s, l}
+}
+
+console.log(rgbToHsl(0,0,128))
--- packages/Theming/src/shade.ts
+++ packages/Theming/src/shade.ts
@@ -1,37 +1,16 @@
 import { hexToRgba } from './rgba'
 
-/**
- * Shades a color(lighten, darken) by a given
- * number, wich can be positive or negative
- *
- * @param   lum The ammount of luminosity
- * @param   hex The color that will be changed
- * @returns hex  The result of the change
- */
-export const shade = (lum: number = 0, color: string): string => {
-  /** Assuming the color is an rgb */
-  let [r, g, b] = color
-    .replace(/[^\d,]/g, '') /** Removes the letters */
-    .split(',') /** Splits the digits by each ',' */
-    .map(x => Number(x)) /** Transforms strings into numbers */
+export const lighten = (lum: number, color: string) => {
+  /** Take the lightness of the hsl color */
+  let hsl: any = color
+  hsl = hsl.replace(/[^\d,]/g, '').split(',')
 
-  /** Otherwise if we redive an HEX color, we transform it to rgb */
-  if (color[0] == '#') {
-    const { r: R, g: G, b: B } = hexToRgba(color)
-    r = R, g = G, b = B
-  }
-  
-  /** Loops for all color numbers, and shades them accordingly */
-  return '#' + [r, g, b].map((_color: any) => {
-    /** Helper function to keep the number between 0 and 255(color limits) */
-    const limit = number => Math.round(Math.min(Math.max(0, number), 255))
+  /** Put the lightness as a value */
+  let _lum: number = lum * 100
+  let _hsl: number = Number(hsl[2]) + _lum
 
-    /** Increase/decrease the value of the numeric shade */
-    const change = _color * lum
+  /** Round the lightness if it passes 100 */
+  if (_hsl > 100) _hsl = 100
 
-    /** Sum the base color with the difference, then return */
-    _color = limit(_color + change).toString(16)
-
-    return _color.length == 2 ? _color : '0' + _color
-  }).join('') /** Merge the three 2-digit colors */
+  return `hsl(${hsl[0]}, ${hsl[1]}%, ${_hsl}%)`
 }
