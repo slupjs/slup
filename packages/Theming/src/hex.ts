84de908fe4713ef1e9c0e9455de4484efba05c84
Gejsi <vgejsi@gmail.com>
2017-10-16 22:19:47 +0200

    :lipstick: Add the rgb conversion in the lighten function

--- packages/Theming/src/hex.ts
+++ packages/Theming/src/hex.ts
@@ -29,7 +29,7 @@ export const rgbToHex = (r: number, g: number, b: number): string =>
   /** Merge the array into a 6-digit HEX string */
   .join('')
 
-export const rgbToHsl = (r: number, g: number, b: number): object => {
+export const rgbToHsl = (r: number, g: number, b: number) => {
   /** Get the values from the rgb color */
   let red: number   = r / 255
   let green: number = g / 255
@@ -38,7 +38,7 @@ export const rgbToHsl = (r: number, g: number, b: number): object => {
   /** Take the max and the min from values */
   let max: number   = Math.max(red, green, blue)
   let min: number   = Math.min(red, green, blue)
-  let delta: number = max - min
+  let delta: number = (max - min)
 
   /** Declare the hsl values that need to be set */
   let h: number, s: number, l: number
@@ -47,10 +47,10 @@ export const rgbToHsl = (r: number, g: number, b: number): object => {
   l = Math.round(((max + min) / 2) * 100)
 
   /** Set the saturation */
-  if (delta == 0) {
+  if (l == 1) {
     s = 0
   } else {
-    s = Math.abs(delta / (1 - Math.abs((2 * l) - 1)))
+    s = l > 0.5 ? delta / (2 - max - min) : delta / (max + min)
   }
 
   /** Set the hue */
@@ -58,6 +58,8 @@ export const rgbToHsl = (r: number, g: number, b: number): object => {
     h = 0
   } else if (red >= blue && blue > green) {
     h = 60 * (6 - ((blue - green) / (red - green)))
+  } else if (red >= green && green >= blue) {
+    h = 60 * ((green - blue) / (red - blue))
   } else if (max == green) {
     h = 60 * (((blue - red) / delta) + 2)
   } else if (max == blue) {
@@ -65,10 +67,10 @@ export const rgbToHsl = (r: number, g: number, b: number): object => {
   }
 
   /** Round the saturation value */
-  let _s = Math.round(s * 10000)
+  let _s = Math.round(s * 100)
   if (_s > 100) _s = 100
 
-  return {h, s: _s, l}
-}
+  let _h = Math.round(h)
 
-console.log(rgbToHsl(0,0,128))
+  return {h: _h, s: _s, l}
+}
